e775bbf3ff63d5bdff81f5cd9743ec375a6272b8
James <wayperayp@gmail.com>
2020-05-12 17:20:01 +0800

    edited password.js

--- middleware/password.js
+++ middleware/password.js
@@ -1,27 +1,32 @@
-const { hash, compare } = require("bcrypt");
-const { getStudent } = require("../models/entities/student");
+ 
+const { hash, compare } = require('bcrypt')
+const { getStudent } = require('../models/entities/student')
 
 const hashPassword = async (req, res, next) => {
-  try {
-    req.body.password = await hash(req.body.password, 10);
-    next();
-  } catch (e) {
-    res.status(500).send({ message: `Can't process account.` });
-  }
-};
-
-const verifyPassword = async (req, res, next) => {
-  const id = req.body.id;
-  const info = await getStudent(id);
-  try {
-    if (await compare(req.body.password, info.Password)) {
-      next();
-    } else {
-      res.status(401).send({ message: `Wrong Password.` });
+    try {
+        req.body.password = await hash(req.body.password, 10)
+        next()
+    } catch {
+        res.status(500).send({ message: `Can't process account.` })
     }
-  } catch (e) {
-    res.status(500).send({ message: `Can't verify account.` });
-  }
-};
+}
 
-module.exports = { hashPassword, verifyPassword };
+const verifyPassword = async(req, res, next) => {
+    const id = req.body.id
+    const info = await getStudent(id)
+    if(info){
+        try {
+            if(await compare(req.body.password, info.Password)){
+                next()
+            } else {
+                res.status(401).send({ message: `Wrong Password.` })
+            }
+        } catch {
+            res.status(500).send({ message: `Can't verify account.` })
+        }
+    } else {
+        res.status(404).send({ message: `User not found.` })
+    }
+}
+
+module.exports = { hashPassword, verifyPassword }
