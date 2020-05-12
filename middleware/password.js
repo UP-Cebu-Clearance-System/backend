1dca9b806a33b80e98a74192b7d328a6dbfcaa64
iamkentleom <leomakibulan@gmail.com>
2020-05-12 16:20:26 +0800

    refactor(password): check if user exist

--- middleware/password.js
+++ middleware/password.js
@@ -13,14 +13,18 @@ const hashPassword = async (req, res, next) => {
 const verifyPassword = async(req, res, next) => {
     const id = req.body.id
     const info = await getStudent(id)
-    try {
-        if(await compare(req.body.password, info.Password)){
-            next()
-        } else {
-            res.status(401).send({ message: `Wrong Password.` })
+    if(info){
+        try {
+            if(await compare(req.body.password, info.Password)){
+                next()
+            } else {
+                res.status(401).send({ message: `Wrong Password.` })
+            }
+        } catch {
+            res.status(500).send({ message: `Can't verify account.` })
         }
-    } catch {
-        res.status(500).send({ message: `Can't verify account.` })
+    } else {
+        res.status(404).send({ message: `User not found.` })
     }
 }
 
