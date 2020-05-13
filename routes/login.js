6c4161f91c6195d8856ea7d9c767e130de664eae
iamkentleom <leomakibulan@gmail.com>
2020-05-13 11:19:06 +0800

    feat(login, signup): separate login and signup for approver and student

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,13 @@
 const login = require('express').Router()
 const { getToken } = require('../middleware/jwt')
 const { verifyPassword } = require('../middleware/password')
+const { approverInfo, studentInfo } = require('../middleware/account')
 
-login.post('/', verifyPassword, getToken, (req, res, next) => {
+login.post('/student', studentInfo, verifyPassword, getToken, (req, res, next) => {
+    res.send({ accessToken: req.body.accessToken })
+})
+
+login.post('/approver', approverInfo, verifyPassword, getToken, (req, res, next) => {
     res.send({ accessToken: req.body.accessToken })
 })
 
--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,16 @@
 const signup = require('express').Router()
+const { hashPassword } = require('../middleware/password')
+const { createStudent } = require('../models/entities/student')
+const { createApprover } = require('../models/entities/approver')
 
-signup.post('/', (req, res) => {
-    res.send(201)
+signup.post('/student', hashPassword, async(req, res) => {
+    const result = await createStudent(req.body.id, req.body.name, 'NULL', req.body.collegeID, req.body.password)
+    res.send(result)
+})
+
+signup.post('/approver', hashPassword, async(req, res) => {
+    const result = await createApprover(req.body.id, req.body.name, req.body.employeeID, req.body.title, req.body.officeID, req.body.password)
+    res.send(result)
 })
 
 module.exports = { signup }
