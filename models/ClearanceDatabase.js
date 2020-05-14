dfceffc04cb173160764a9392cda6d35c8990175
James <wayperayp@gmail.com>
2020-05-14 11:29:52 +0800

    added CID field to Clearance Table

--- models/ClearanceDatabase.js
+++ models/ClearanceDatabase.js
@@ -1,4 +1,11 @@
 const Student = require("./entities/student");
+const Office = require("./entities/office");
+const ClearanceType = require("./entities/clearance-type");
+const College = require("./entities/college");
+const Clearance = require("./entities/clearance");
+const ClearanceFlow = require("./entities/clearance-flow");
+const Approver = require("./entities/approver");
+const Admin = require("./entities/admin");
 
 const getAllStudents = async () => {
   return await Student.getAllStudents();
