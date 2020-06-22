334877ea2457a2e246a9fdec7fb80d41b0c4866b
iamkentleom <leomakibulan@gmail.com>
2020-06-22 14:48:13 +0800

    feat(approver, clearance): interaction features
    - Approver
        - see the queue of students that are applying for signatures
        - sign students
        - reject students
    
    - Clearance
        - better clearance list with approver name and title
        - apply for signature of approver
        - cancel application for signature

--- routes/approver.js
+++ routes/approver.js
@@ -1,8 +1,47 @@
 const approver = require('express').Router()
 const { verifyToken } = require('../middleware/jwt')
+const { approverFetchClearanceQueue,  approverSignClearance, approverSignClearanceWithRemarks, approverRejectClearance, approverRestoreClearable } = require('../models/ClearanceDatabase')
+const { getClearanceInfoFromCID } = require('../models/tables/clearance')
 
-approver.get('/', verifyToken, (req, res) => {
-    res.status(200).send({ message: `Approver: ${ req.body.id }` })
+approver.get('/', verifyToken, async(req, res) => {
+    const queue = await approverFetchClearanceQueue(req.body.id)
+    res.status(200).send(queue)
 })
+
+approver.post('/sign', verifyToken, async(req, res) => {
+    const id = req.body.id
+    const cid = req.body.CID
+    const fbi = (await getClearanceInfoFromCID(cid)).ApproverID
+    if(id === fbi){
+        const message = req.body.remarks ? await approverSignClearanceWithRemarks(cid, req.body.remarks) : await approverSignClearance(cid)
+        res.status(200).send(message)
+    }else{
+        res.status(403).send({ message: 'Signing forbidden.' })
+    }
+})
+
+approver.post('/reject', verifyToken, async(req, res) => {
+    const id = req.body.id
+    const cid = req.body.CID
+    const fbi = (await getClearanceInfoFromCID(cid)).ApproverID
+    if(id === fbi){
+        const message = await approverRejectClearance(cid, req.body.remarks)
+        res.status(200).send(message)
+    }else{
+        res.status(403).send({ message: 'Signing forbidden.' })
+    }
+})
+
+// approver.post('/restore', verifyToken, async(req, res) => {
+//     const id = req.body.id
+//     const cid = req.body.CID
+//     const fbi = (await getClearanceInfoFromCID(cid)).ApproverID
+//     if(id === fbi){
+//         const message = req.body.remarks ? await approverSignClearanceWithRemarks(cid, req.body.remarks) : await approverSignClearance(cid)
+//         res.status(200).send(message)
+//     }else{
+//         res.status(403).send({ message: 'Signing forbidden.' })
+//     }
+// })
 
 module.exports = { approver }
