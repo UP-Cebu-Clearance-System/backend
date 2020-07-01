ab56a248d7f90d5c4ffe505531cf31bf07bf73d9
iamkentleom <leomakibulan@gmail.com>
2020-07-01 19:26:30 +0800

    refactor(approver): proceed to next after sign
    - students will automatically proceed to the next approver after signed
      by the current approver (handledd by applyNext middleware)
    - student info updated if cleared

--- middleware/next.js
+++ middleware/next.js
@@ -1,4 +1,6 @@
-const { fetchClearance, fetchStudentInfo } = require('../models/ClearanceDatabase')
+const { fetchClearance, fetchStudentInfo, studentApplyClearable } = require('../models/ClearanceDatabase')
+const { getClearanceInfoFromCID } = require('../models/tables/clearance')
+const { getStudentInfoFromClearanceID, updateStatus } = require('../models/tables/student')
 
 const CIDNext = async(req, res, next) => {
     try {
@@ -14,8 +16,20 @@ const CIDNext = async(req, res, next) => {
     }
 }
 
-const applyNext = async(req, res, next) => {
-
+const applyNext = async(req, res) => {
+    const { ClearanceID } = await getClearanceInfoFromCID(req.body.CID)
+    const { StudentID } = await getStudentInfoFromClearanceID(ClearanceID)
+    const flow = await fetchClearance(ClearanceID)
+    let ans = flow.map(x => ({ CID: x.CID, Status: x.Status }))
+    ans = ans.filter(x => x.Status !== 'Signed')[0]
+    console.log(StudentID, ans)
+    if(ans !== undefined){
+        await studentApplyClearable(StudentID, ans.CID)
+        res.status(200).send({ message: 'Successfully signed.' })
+    }else{
+        await updateStatus('Cleared', StudentID)
+        res.status(200).send({ message: 'Successfully signed.' })
+    }
 }
 
 module.exports = { applyNext, CIDNext }
--- routes/approver.js
+++ routes/approver.js
@@ -1,35 +1,36 @@
 const approver = require('express').Router()
 const { approverFetchClearanceQueue,  approverSignClearance, approverSignClearanceWithRemarks, approverRejectClearance, approverRestoreClearable } = require('../models/ClearanceDatabase')
 const { getClearanceInfoFromCID } = require('../models/tables/clearance')
+const { applyNext } = require('../middleware/next')
 
 approver.get('/', async(req, res) => {
     const queue = await approverFetchClearanceQueue(req.body.id)
     res.status(200).send(queue)
 })
 
-approver.post('/sign', async(req, res) => {
+approver.post('/sign', async(req, res, next) => {
     const id = req.body.id
     const cid = req.body.CID
     const fbi = (await getClearanceInfoFromCID(cid)).ApproverID
     if(id === fbi){
         const message = req.body.remarks ? await approverSignClearanceWithRemarks(cid, req.body.remarks) : await approverSignClearance(cid)
-        res.status(200).send(message)
+        next()
     }else{
         res.status(403).send({ message: 'Signing forbidden.' })
     }
-})
+}, applyNext)
 
-approver.post('/reject', async(req, res) => {
+approver.post('/reject', async(req, res, next) => {
     const id = req.body.id
     const cid = req.body.CID
     const fbi = (await getClearanceInfoFromCID(cid)).ApproverID
     if(id === fbi){
         const message = await approverRejectClearance(cid, req.body.remarks)
-        res.status(200).send(message)
+        next()
     }else{
         res.status(403).send({ message: 'Signing forbidden.' })
     }
-})
+}, applyNext)
 
 // approver.post('/restore', async(req, res) => {
 //     const id = req.body.id
