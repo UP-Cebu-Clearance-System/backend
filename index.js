e9320fc8937d228ec2713d21969e5507cf5b40ef
iamkentleom <leomakibulan@gmail.com>
2020-07-02 21:37:28 +0800

    feat(index, gitignore): access logging
    - ignore logs folder
    - create logger
        - apache common log in production
        - morgan dev in development

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
+const { createStream } = require('rotating-file-stream')
 const { routes } = require('./routes')
 
 const app = express()
@@ -8,6 +10,15 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 const port = process.env.PORT || 3000
+
+if(process.env.NODE_ENV === 'production'){
+    const accessLogStream = createStream('access.log', {
+        interval: '1d',
+        path: `${ __dirname }/logs`
+    })
+    app.use(morgan(':remote-addr - :user-agent [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]', { stream: accessLogStream }))
+}
+else app.use(morgan('dev'))
 
 app.use(cors())
 app.use(express.json())
