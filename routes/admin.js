a50b8b64b25ba26f8d6eeb675017494a5f15fc00
iamkentleom <leomakibulan@gmail.com>
2020-07-04 23:05:38 +0800

    feat(admin): backup and logs
    - display all recorded log files with their download url
    - download specific log file
    - download a backup of database

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,23 @@
 const admin = require('express').Router()
+const { readdirSync } = require('fs')
 const {} = require('../models/Admin')
 
 admin.post('/add/approver', (req, res) => {
     res.status(200).send({ message: 'this is the admin' })
+})
+
+admin.get('/download/backup', (req, res) => {
+    res.download('clearance.sqlite')
+})
+
+admin.get('/download/log/:file', (req, res) => {
+    res.download(`logs/${ req.params.file }`)
+})
+
+admin.get('/logs', (req, res) => {
+    const files = readdirSync('logs/')
+    const baseURL = `${ req.protocol }://${ req.get('host') }/admin/download/log/`
+    res.status(200).send(files.map(filename => ({ filename, url: encodeURI(baseURL + filename) })))
 })
 
 module.exports = { admin }
