1f614296eb526bf052d43d80e976329b47ed4f20
iamkentleom <leomakibulan@gmail.com>
2020-07-04 22:52:03 +0800

    fix(jwt): admin username as id in jwt payload

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,7 +1,7 @@
 const { sign, verify } = require('jsonwebtoken')
 
 const getToken = (req, res, next) => {
-    const payload = { id: req.body.id, role: req.route.path.split('/')[1] }
+    const payload = { id: req.body.id || req.body.username, role: req.route.path.split('/')[1] }
     sign(payload, process.env.SECRET, { expiresIn: '1d' }, (err, token) => {
         if(err){
             res.status(500).send({ message: `Error getting token.` })
