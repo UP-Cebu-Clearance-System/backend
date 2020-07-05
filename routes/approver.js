9c0e3ae77bbad9a50d8896336a4c6f2959b69ec0
iamkentleom <leomakibulan@gmail.com>
2020-07-05 13:03:21 +0800

    feat(approver): restore approver's actions
    - show logs of approver activities
    - restore to a specific restore point (from the list of approver's
      actions)

--- routes/approver.js
+++ routes/approver.js
@@ -1,6 +1,7 @@
 const approver = require('express').Router()
 const { approverFetchClearanceQueue,  approverSignClearance, approverSignClearanceWithRemarks, approverRejectClearance, approverRestoreClearable } = require('../models/ClearanceDatabase')
 const { getClearanceInfoFromCID } = require('../models/tables/clearance')
+const { getClearanceLog } = require('../models/tables/clearance-log')
 const { applyNext } = require('../middleware/next')
 
 approver.get('/', async(req, res) => {
@@ -28,20 +29,22 @@ approver.post('/reject', async(req, res, next) => {
         const message = await approverRejectClearance(cid, req.body.remarks)
         next()
     }else{
-        res.status(403).send({ message: 'Signing forbidden.' })
+        res.status(403).send({ message: 'Rejecting forbidden.' })
     }
 }, applyNext)
 
-// approver.post('/restore', async(req, res) => {
-//     const id = req.body.id
-//     const cid = req.body.CID
-//     const fbi = (await getClearanceInfoFromCID(cid)).ApproverID
-//     if(id === fbi){
-//         const message = req.body.remarks ? await approverSignClearanceWithRemarks(cid, req.body.remarks) : await approverSignClearance(cid)
-//         res.status(200).send(message)
-//     }else{
-//         res.status(403).send({ message: 'Signing forbidden.' })
-//     }
-// })
+approver.get('/logs', async(req, res) => {
+    const log = await getClearanceLog(req.body.id)
+    res.status(200).send(log)
+})
+
+approver.post('/restore', async(req, res) => {
+    const log = (await getClearanceLog(req.body.id)).map(x => x.ID)
+    if(log.includes(req.body.logID)){
+        const message = approverRestoreClearable(req.body.logID)
+        res.status(200).send(message)
+    }
+    else res.status(403).send({ message: 'Restoring forbidden.' })
+})
 
 module.exports = { approver }
