b4423fa6b21464ea65e26f221c3ed7f354b945d6
iamkentleom <leomakibulan@gmail.com>
2020-07-07 20:38:44 +0800

    feat(admin): admin control for approver
    - manual query for admin
    - approver related admin controls [add approver, change approver
      title/name, ...]

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,29 @@
 const admin = require('express').Router()
 const { readdirSync } = require('fs')
-const {} = require('../models/Admin')
+const { adminAddApprover, adminAddApproverTitle, adminApproverUpdateName, adminApproverUpdateTitle, adminManualQuery } = require('../models/Admin')
 
-admin.post('/add/approver', (req, res) => {
-    res.status(200).send({ message: 'this is the admin' })
+admin.post('/add/approver', async(req, res) => {
+    const message = await adminAddApprover(req.body.name, req.body.title, req.body.officeID, req.body.password)
+    res.status(201).send(message)
+})
+
+admin.post('/add/approverTitle', async(req, res) => {
+    const message = await adminAddApproverTitle(req.body.approverID, req.body.password)
+    res.status(201).send(message)
+})
+
+admin.post('/add/clearanceFlow', (req, res) => {
+
+})
+
+admin.post('/approver/updateName', async(req, res) => {
+    const message = await adminApproverUpdateName(req.body.approverID, req.body.name)
+    res.status(200).send(message)
+})
+
+admin.post('/approver/updateTitle', async(req, res) => {
+    const message = await adminApproverUpdateTitle(req.body.approverID, req.body.title)
+    res.status(200).send(message)
 })
 
 admin.get('/download/backup', (req, res) => {
@@ -18,6 +38,11 @@ admin.get('/logs', (req, res) => {
     const files = readdirSync('logs/')
     const baseURL = `${ req.protocol }://${ req.get('host') }/admin/download/log/`
     res.status(200).send(files.map(filename => ({ filename, url: encodeURI(baseURL + filename) })))
+})
+
+admin.post('/query', async(req, res) => {
+    const message = await adminManualQuery(req.body.query)
+    res.status(200).send(message)
 })
 
 module.exports = { admin }
